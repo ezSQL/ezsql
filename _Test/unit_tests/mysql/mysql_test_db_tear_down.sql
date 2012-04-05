32714ae168149bff19fde1042740110fd903cc75
sjstoelting <mail@stefanie-stoelting.de>
2012-04-05 20:01:01 +0200

    Changed database tear up and tear down scripts for MySQL. You need to run the tear up script manually before you start the unit tests.

--- _Test/unit_tests/mysql/mysql_test_db_tear_down.sql
+++ _Test/unit_tests/mysql/mysql_test_db_tear_down.sql
@@ -2,15 +2,14 @@
  * Tear down script to remove all test objects after the test
  *
  * @author  Stefanie Janine Stoelting (mail@stefanie-stoelting.de)
- * 
  * @name    ezSQL_mysql_tear_down
  * @package ezSQL
+ * @subpackage unitTests
  * @license FREE / Donation (LGPL - You may do what you like with ezSQL - no exceptions.)
+ */
 
-*/
+-- Drop the user
+DROP USER ezTest@localhost;
 
 -- Drop the database
 DROP DATABASE ez_test;
-
--- Drop the user
-DROP USER ezTest;
