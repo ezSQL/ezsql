fb248d601b42ff6f8a0c0941e520f79a291f9a74
techno-express <technoexpressnet@gmail.com>
2018-03-08 16:42:15 -0500

    testing

--- postgresql/ez_sql_postgresql.php
+++ postgresql/ez_sql_postgresql.php
@@ -269,7 +269,8 @@
 			{	
 				$this->dbh = $this->connect($this->dbuser, $this->dbpassword, $this->dbname, $this->dbhost, $this->port);
 			}
-
+            
+            pg_query($this->dbh, "SELECT lastval()")
 			// Perform the query via std postgresql_query function..
 			$this->result = @pg_query($this->dbh, $query);
 
