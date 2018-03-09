2d1a2ba582568e824bdaf5131b44babe316a3e53
techno-express <technoexpressnet@gmail.com>
2018-03-08 21:20:37 -0500

    corrections

--- tests/postgresql/ezSQL_postgresqlTest.php
+++ tests/postgresql/ezSQL_postgresqlTest.php
@@ -227,7 +227,7 @@ class ezSQL_postgresqlTest extends TestCase {
         
         $where['id'] = '3';
         $where['test_value'] = 'testing string 3';
-        $result = $this->object->showing('unit_test', 'test_key', $where ));
+        $result = $this->object->showing('unit_test', 'test_key', $where);
         foreach ($result as $row) {
             $this->assertEquals('test 3', $row->test_key);
         }      
