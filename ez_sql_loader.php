70ea56573ea95b720fc1f1ecf9d6aee276d48631
techno-express <technoexpressnet@gmail.com>
2018-03-14 23:12:27 -0400

    moved all sql driver class into lib single folder

--- ez_sql_loader.php
+++ ez_sql_loader.php
@@ -2,7 +2,7 @@
 require_once('shared'.DIRECTORY_SEPARATOR.'ez_sql_core.php');
 function ezsql_autoloader($class) {    
 	$foldername = ltrim($class, 'ezSQL_');
-    $file = strtolower($foldername).DIRECTORY_SEPARATOR.'ez_sql_'.strtolower($foldername).'.php';
+    $file = 'lib'.DIRECTORY_SEPARATOR.'ez_sql_'.strtolower($foldername).'.php';
     if (file_exists($file)) {
         require_once($file);
     }
