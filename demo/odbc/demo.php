a7549b7fe251b8dcd72811804cad90a2706e693f
techno-express <technoexpressnet@gmail.com>
2018-03-28 07:38:55 -0400

    build corrections

--- demo/odbc/demo.php
+++ demo/odbc/demo.php
@@ -4,7 +4,7 @@
  */
 
 	/**********************************************************************
-	*  ezSQL initialisation for mySQL
+	*  ezSQL initialization for MS-SQL
 	*/
 
 	// Include ezSQL core
@@ -12,22 +12,37 @@
 
 	// Initialise database object and establish a connection
 	// at the same time - db_user / db_password / db_name / db_host
-	$db = new ezSQL_mysql('db_user','db_password','db_name','db_host');
+
+
+
+	 $db_host = '';
+	 $db_name = '';
+	 $db_user = '';
+	 $db_password = '';
+
+	 //example
+	 //$db_host = 'servername';   or $db_host = 'servername, portnumber'
+	 //$db_name = 'AdventureWorks';
+	 //$db_user = 'sa';
+	 //$db_password = 'password';
+
+
+	$db = new ezSQL_odbc($db_user, $db_password, $db_name, $db_host);
 
 	/**********************************************************************
-	*  ezSQL demo for mySQL database
+	*  ezSQL demo for MS-Sql database
 	*/
 
 	// Demo of getting a single variable from the db
 	// (and using abstracted function sysdate)
 	$current_time = $db->get_var("SELECT " . $db->sysdate());
-	print "ezSQL demo for mySQL database run @ $current_time";
+	print "ezSQL demo for mS-SQL database run @ $current_time";
 
 	// Print out last query and results..
 	$db->debug();
 
 	// Get list of tables from current database..
-	$my_tables = $db->get_results("SHOW TABLES",ARRAY_N);
+	$my_tables = $db->get_results("select name from ".$db_name."..sysobjects where xtype = 'U'",ARRAY_N);
 
 	// Print out last query and results..
 	$db->debug();
@@ -36,7 +51,7 @@
 	foreach ( $my_tables as $table )
 	{
 		// Get results of DESC table..
-		$db->get_results("DESC $table[0]");
+		$db->query("EXEC SP_COLUMNS '".$table[0]."'");
 
 		// Print out last query and results..
 		$db->debug();
