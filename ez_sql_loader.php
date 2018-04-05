c108eea3e48ea8aed41368811ca18cc5b946c966
techno-express <technoexpressnet@gmail.com>
2018-04-04 22:15:00 -0400

    update autoloader to use absolute directory structure

--- ez_sql_loader.php
+++ ez_sql_loader.php
@@ -1,8 +1,8 @@
 <?php
-require_once('shared'.DIRECTORY_SEPARATOR.'ez_sql_core.php');
+require_once(__DIR__ .DIRECTORY_SEPARATOR.'shared'.DIRECTORY_SEPARATOR.'ez_sql_core.php');
 function ezsql_autoloader($class) {    
 	$foldername = ltrim($class, 'ezSQL_');
-    $file = 'lib'.DIRECTORY_SEPARATOR.'ez_sql_'.strtolower($foldername).'.php';
+    $file = __DIR__ .DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'ez_sql_'.strtolower($foldername).'.php';
     if (file_exists($file)) {
         require_once($file);
     }
