360d8a5ad3799e3c251b337477725914fcc85366
L. Stubbs <technoexpressnet@gmail.com>
2018-09-15 14:41:48 -0400

    Fix constructor parameters not being passed, was only working for Closure.

--- lib/Container.php
+++ lib/Container.php
@@ -3,6 +3,7 @@ namespace ezsql\Container;
 
 /**
  * Dependency Injection Container 
+ * @see https://gist.github.com/MustafaMagdi/2bb27aebf6ab078b1f3e5635c0282fac
  * 
  */
 trait Container
@@ -26,34 +27,34 @@ trait Container
 
 	/**
 	 * @param       $abstract
-	 * @param array $parameters
+	 * @param array $values
 	 *
 	 * @return mixed|null|object
 	 * @throws Exception
 	 */
-	public function get($abstract, $parameters = [])
+	public function get($abstract, $values = [])
 	{
 		// if we don't have it, just register it
 		if (!isset($this->instances[$abstract])) {
 			$this->set($abstract);
 		}
 
-		return $this->resolve($this->instances[$abstract], $parameters);
+		return $this->resolve($this->instances[$abstract], $values);
 	}
 
 	/**
 	 * resolve single
 	 *
 	 * @param $concrete
-	 * @param $parameters
+	 * @param $values
 	 *
 	 * @return mixed|object
 	 * @throws Exception
 	 */
-	public function resolve($concrete, $parameters)
+	public function resolve($concrete, $values = [])
 	{
 		if ($concrete instanceof Closure) {
-			return $concrete($this, $parameters);
+			return $concrete($this, $values);
 		}
 
 		$reflector = new ReflectionClass($concrete);
@@ -71,7 +72,7 @@ trait Container
 
 		// get constructor params
 		$parameters   = $constructor->getParameters();
-		$dependencies = $this->getDependencies($parameters);
+		$dependencies = $this->getDependencies($parameters, $values);
 
 		// get new instance with dependencies resolved
 		return $reflector->newInstanceArgs($dependencies);
@@ -85,23 +86,29 @@ trait Container
 	 * @return array
 	 * @throws Exception
 	 */
-	public function getDependencies($parameters)
+	public function getDependencies($parameters, $values)
 	{
 		$dependencies = [];
 		foreach ($parameters as $parameter) {
 			// get the type hinted class
 			$dependency = $parameter->getClass();
-			if ($dependency === NULL) {
-				// check if default value for a parameter is available
-				if ($parameter->isDefaultValueAvailable()) {
+			if ($dependency === NULL) {				
+				// check if the constructor parameter name exists as a key in the values array
+				if (array_key_exists($parameter->getName(), $values)) {			  
+				  // get default value of parameter
+				  $dependencies[] = $values[$parameter->getName()];			  
+				} else {			  
+				  // check if default value for a parameter is available
+				  if ($parameter->isDefaultValueAvailable()) {			  
 					// get default value of parameter
-					$dependencies[] = $parameter->getDefaultValue();
-				} else {
-					throw new Exception("Can not resolve class dependency {$parameter->name}");
-				}
-			} else {
+					$dependencies[] = $parameter->getDefaultValue();			  
+				  } else {			  
+					throw new Exception("Can not resolve class dependency {$parameter->name}");			  
+				  }			  
+				}							  
+			} else {			  
 				// get dependency resolved
-				$dependencies[] = $this->get($dependency->name);
+				$dependencies[] = $this->get($dependency->name);			  
 			}
 		}
 
