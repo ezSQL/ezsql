19b195f2e69447865944772419a524e0ab94f496
L. Stubbs <technoexpressnet@gmail.com>
2019-03-04 12:18:04 -0500

    Update DT.php

--- shared/DT.php
+++ shared/DT.php
@@ -92,23 +92,27 @@ class DT
 			$store = !empty($args[0]) ? '('.$args[0].')' : '';
 			$value = !empty($args[1]) ? $args[1] : '';
 			$options = !empty($args[2]) ? $args[2] : '';
-			$data = $type.$store.' '.$value.' '.$options;
+			$extra = !empty($args[3]) ? ' '.$args[3] : '';
+			$data = $type.$store.' '.$value.' '.$options.$extra;
 		} elseif (\preg_match($numericPattern, $type)) {
 			// check for numeric data type
 			$size = '('.(!empty($args[0]) ? $args[0] : '6').',';
 			$size .= (!empty($args[1]) ? $args[1] : '2').') ';
 			$value = !empty($args[1]) ? $args[1] : '';
 			$options = !empty($args[2]) ? $args[2] : '';
-			$data = $type.$size.' '.$value.' '.$options;
+			$extra = !empty($args[3]) ? ' '.$args[3] : '';
+			$data = $type.$size.' '.$value.' '.$options.$extra;
         } elseif (\preg_match($dateTimePattern, $type)) {
 			// check for date time data type
 			$fraction = !empty($args[0]) ? '('.$args[0].')' : '';
 			$value = !empty($args[1]) ? $args[1] : '';
 			$options = !empty($args[2]) ? $args[2] : '';
-			$data = $type.$fraction.' '.$value.' '.$options;
+			$extra = !empty($args[3]) ? ' '.$args[3] : '';
+			$data = $type.$fraction.' '.$value.' '.$options.$extra;
         } elseif (\preg_match($objectPattern, $type)) {
 			// check for large object data type
-			$data = $type.(!empty($args[0]) ? ' '.$args[0] : '');
+			$value = !empty($args[0]) ? ' '.$args[0] : '';
+			$data = $type.$value;
         }
 
 		return $data;
