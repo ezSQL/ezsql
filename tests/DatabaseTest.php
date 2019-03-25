8d9153d9a645a7490a62488e59ed1b9be389683e
techno-express <technoexpressnet@gmail.com>
2019-03-25 11:13:49 -0400

    fix build errors

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -18,6 +18,12 @@ class DatabaseTest extends EZTestCase
     */
     public function testInitialize()
     {
+        if (!extension_loaded('mysqli')) {
+            $this->markTestSkipped(
+              'The MySQLi extension is not available.'
+            );
+        }
+
         $mysqli = Database::initialize(MYSQLI, [self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME]);
         $this->assertFalse($mysqli instanceof ConfigInterface);
         $this->assertTrue($mysqli->settings() instanceof ConfigInterface);
@@ -32,6 +38,12 @@ class DatabaseTest extends EZTestCase
     */
     public function testInitialize_Pgsql()
     {
+        if (!extension_loaded('pgsql')) {
+            $this->markTestSkipped(
+              'The PostgreSQL Lib is not available.'
+            );
+        }
+
         $pgsql = Database::initialize(PGSQL, [self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME, self::TEST_DB_HOST, self::TEST_DB_PORT]);
         $this->assertFalse($pgsql instanceof ConfigInterface);
         $this->assertTrue($pgsql->settings() instanceof ConfigInterface);
@@ -46,6 +58,12 @@ class DatabaseTest extends EZTestCase
     */
     public function testInitialize_Sqlite3()
     {
+        if (!extension_loaded('sqlite3')) {
+            $this->markTestSkipped(
+              'The sqlite3 Lib is not available.'
+            );
+        }
+        
         $sqlite3 = Database::initialize(SQLITE3, [self::TEST_SQLITE_DB_DIR, self::TEST_SQLITE_DB]);
         $this->assertFalse($sqlite3 instanceof ConfigInterface);
         $this->assertTrue($sqlite3->settings() instanceof ConfigInterface);
@@ -60,6 +78,12 @@ class DatabaseTest extends EZTestCase
     */
     public function testInitialize_Sqlsrv()
     {
+        if (!extension_loaded('sqlsrv')) {
+            $this->markTestSkipped(
+              'The sqlsrv Lib is not available.'
+            );
+        }
+
         $sqlsrv = Database::initialize(SQLSERVER, [self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME]);
         $this->assertFalse($sqlsrv instanceof ConfigInterface);
         $this->assertTrue($sqlsrv->settings() instanceof ConfigInterface);
@@ -74,6 +98,12 @@ class DatabaseTest extends EZTestCase
     */
     public function testInitialize_Pdo()
     {
+        if ( ! \class_exists ('PDO') ) {
+            $this->markTestSkipped(
+              'The PDO Lib is not available.'
+            );
+        }
+
         $pdo = Database::initialize(Pdo, 
             ['mysql:host='.self::TEST_DB_HOST.';dbname='.self::TEST_DB_NAME.';port=3306', self::TEST_DB_USER,self::TEST_DB_PASSWORD]);
         $this->assertFalse($pdo instanceof ConfigInterface);
