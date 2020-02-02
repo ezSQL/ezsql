8d37ca7f07dd0358fff4145d4669f83aaa2d45cc
techno-express <technoexpressnet@gmail.com>
2020-02-02 10:35:45 -0500

    Update Constants.php

--- lib/Constants.php
+++ lib/Constants.php
@@ -5,7 +5,7 @@ if (!\defined('CONSTANTS')) {
     /**
      * ezsqlModel Constants
      */
-    \defined('EZSQL_VERSION') or \define('EZSQL_VERSION', '4.0.10');
+    \defined('EZSQL_VERSION') or \define('EZSQL_VERSION', '4.0.9');
     \defined('OBJECT') or \define('OBJECT', 'OBJECT');
     \defined('ARRAY_A') or \define('ARRAY_A', 'ARRAY_A');
     \defined('ARRAY_N') or \define('ARRAY_N', 'ARRAY_N');
